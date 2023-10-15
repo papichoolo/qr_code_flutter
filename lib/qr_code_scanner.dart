4234456e7691d2b8da4688a43b2d1953c0cfb481
papichoolo <souravsahil100@gmail.com>
2023-10-15 15:43:01 +0530

    improved visibility

--- lib/qr_code_scanner.dart
+++ lib/qr_code_scanner.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'mogo/mongo.dart';
@@ -38,7 +40,7 @@ class _QRScannerScreenState extends State<QRScannerScreen> {
         child: Center(
           child: Text( 
             'Scanned Code: $scannedCode',
-            style:  const TextStyle(fontSize: 15),
+            style:  const TextStyle(fontSize: 20),
             )
              ),
       ),
@@ -57,6 +59,9 @@ class _QRScannerScreenState extends State<QRScannerScreen> {
         onPressed: () {
           //showSuccessPopup(context);
           updateAttendance(scannedCode, context);
+          setState(() {
+            scannedCode = '';
+          });
         },
         style: ElevatedButton.styleFrom(
           foregroundColor: Colors.white,
@@ -69,6 +74,38 @@ class _QRScannerScreenState extends State<QRScannerScreen> {
 
     );
   }
+
+
+Widget _buildQrView(BuildContext context) {
+    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 150.0
+        : 300.0;
+    // To ensure the Scanner view is properly sizes after rotation
+    // we need to listen for Flutter SizeChanged notification and update controller
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+          borderColor: Colors.red,
+          borderRadius: 10,
+          borderLength: 30,
+          borderWidth: 10,
+          cutOutSize: scanArea),
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
+  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+    log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
+    if (!p) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('no Permission')),
+      );
+    }
+  }
+
+
 
 
   void _onQRViewCreated(QRViewController controller){
